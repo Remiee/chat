28b6bc9ff43beb677d1e3be5d1645759301e2069
Remiee <nemes.agi96@gmail.com>
2018-12-06 21:14:20 +0100

    Fix username display

--- public/app.js
+++ public/app.js
@@ -5,19 +5,19 @@ new Vue({
         ws: null, // Our websocket
         newMsg: '', // Holds new messages to be sent to the server
         chatContent: '', // A running list of chat messages displayed on the screen
-        email: null, // Email address used for grabbing an avatar
-        username: null, // Our username
+        username: null,
+        email: null,
         joined: false // True if email and username have been filled in
     },
     created: function () {
         var self = this;
         this.ws = new WebSocket('ws://' + window.location.host + '/ws');
         this.ws.addEventListener('message', function (e) {
-            var msg = JSON.parse(e.data);
+            var msg = JSON.parse(e.data)
             self.chatContent += '<div class="chip">' +
-                '<img src="' + self.gravatarURL(msg.email) + '">' // Avatar
+                '<img src="' + self.gravatarURL(msg.user.email) + '">' // Avatar
                 +
-                msg.username +
+                msg.user.username +
                 '</div>' +
                 emojione.toImage(msg.message) + '<br/>'; // Parse emojis
 
@@ -30,8 +30,10 @@ new Vue({
             if (this.newMsg != '') {
                 this.ws.send(
                     JSON.stringify({
-                        email: this.email,
-                        username: this.username,
+                        user: {
+                            email: this.email,
+                            username: this.username
+                        },
                         message: $('<p>').html(this.newMsg).text() // Strip out html
                     }));
                 this.newMsg = ''; // Reset newMsg
